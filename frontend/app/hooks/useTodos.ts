d7f5ed3784b82b1819316f59e4f1fafe70d0c5d1
Lm09 <123.moharana@gmail.com>
2025-01-10 20:23:50 +0530

    updates-0.1

--- frontend/app/hooks/useTodos.ts
+++ frontend/app/hooks/useTodos.ts
@@ -13,7 +13,9 @@ export function useTodos() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/todos");
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_SERVER_URL}/api/todos`
+        );
         if (!response.ok) throw new Error("Failed to fetch data");
         const data = await response.json();
         setTodos(data.todos);
@@ -32,13 +34,16 @@ export function useTodos() {
     todo: Omit<Todo, "id" | "createdAt" | "completed">
   ) => {
     try {
-      const response = await fetch("http://localhost:5000/api/todos", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(todo),
-      });
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/todos`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(todo),
+        }
+      );
 
       if (!response.ok) throw new Error("Failed to add todo");
       const newTodo = await response.json();
@@ -52,7 +57,7 @@ export function useTodos() {
   const toggleTodo = async (id: string) => {
     try {
       const response = await fetch(
-        `http://localhost:5000/api/todos/${id}/toggle`,
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/todos/${id}/toggle`,
         {
           method: "PATCH",
         }
@@ -72,9 +77,12 @@ export function useTodos() {
 
   const deleteTodo = async (id: string) => {
     try {
-      const response = await fetch(`http://localhost:5000/api/todos/${id}`, {
-        method: "DELETE",
-      });
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/todos/${id}`,
+        {
+          method: "DELETE",
+        }
+      );
 
       if (!response.ok) throw new Error("Failed to delete todo");
       setTodos((prev) => prev.filter((todo) => todo.id !== id));
